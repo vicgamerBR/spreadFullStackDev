8d9e48aafacb916f729119ded29fbc774a8ed448
viquituRheiz <vhreis13@gmail.com>
2022-04-24 14:30:49 -0300

    postagem do desafio do accordion jQuery UI

---
+++ introDevWeb/desafio accordion.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <title>Desafio Accordion</title>
+        <meta charset="utf-8">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        <script type="text/javascript">
+            $(function (){
+                $("#accordion").accordion();
+            });
+        </script>
+        <style type="text/css">
+            body{
+                padding-top:3%;
+                padding-right: 7%;
+                padding-left: 7%;
+                background-color: lightgray;
+            }
+
+            h1{
+                font-family: sans-serif;
+                color: darkgreen;
+                font-size: 50px;
+                text-align: center;
+            }
+
+            h3{
+                font-family: sans-serif;
+                color: darkgreen;
+                font-size: 40px;
+                text-align: center;
+            }
+
+            p{
+                font-family: sans-serif;
+                font-size: 20px;
+                text-align: justify;
+            }
+
+        </style>
+    </head>
+    <body>
+        <h1>Accordion Mussum Ipsum</h1>
+        <div id="accordion">
+            <h3>Parágrafo 1</h3>
+            <div>
+                <p>
+                Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis.Mé faiz elementum girarzis, nisi eros vermeio.Delegadis gente finis, bibendum egestas augue arcu ut est.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+                </p>
+            </div>
+            <h3>Parágrafo 2</h3>
+            <div>
+                <p>
+                Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Si num tem leite então bota uma pinga aí cumpadi!In elementis mé pra quem é amistosis quis leo.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.                </p>
+            </div>
+            <h3>Parágrafo 3</h3>
+            <div>
+                <p>
+                Manduma pindureta quium dia nois paga.Aenean aliquam molestie leo, vitae iaculis nisl.Viva Forevis aptent taciti sociosqu ad litora torquent.Mé faiz elementum girarzis, nisi eros vermeio.                </p>
+            </div>
+            <h3>Parágrafo 4</h3>
+            <div>
+                <p>
+                Aenean aliquam molestie leo, vitae iaculis nisl.Diuretics paradis num copo é motivis de denguis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Interagi no mé, cursus quis, vehicula ac nisi.                </p>
+            </div>
+            <h3>Parágrafo 5</h3>
+            <div>
+                <p>
+                Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.Delegadis gente finis, bibendum egestas augue arcu ut est.Quem num gosta di mé, boa gentis num é.Si num tem leite então bota uma pinga aí cumpadi!                </p>
+            </div>
+            <h3>Parágrafo 6</h3>
+            <div>
+                <p>
+                Per aumento de cachacis, eu reclamis.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.Copo furadis é disculpa de bebadis, arcu quam euismod magna.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.                </p>
+            </div>
+        </div>
+    </body>
+
+</html>
