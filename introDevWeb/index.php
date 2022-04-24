0c82ba9dd77044b144be272ecce6b84ed237c721
viquituRheiz <vhreis13@gmail.com>
2022-04-24 13:53:27 -0300

    início do projeto de aula prática de introdução a desenvolvimento web

---
+++ introDevWeb/index.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        <title>Meu primeiro site em PHP!</title>
+    </head>
+
+    <body>
+
+        <?php
+            for($i = 0; $i < 10; $i++){
+                print("Linha Nº " . $i . "<br />");
+            }
+        ?>
+        
+    </body>
+</html>
