5dde07118f0422ff6aa554873bea636b2555cce3
viquituRheiz <vhreis13@gmail.com>
2022-04-24 14:04:39 -0300

    finalização da aula prática de introdução a desenvolvimento web

--- introDevWeb/index.php
+++ introDevWeb/index.php
@@ -2,15 +2,27 @@
 
     <head>
         <title>Meu primeiro site em PHP!</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <style type="text/css">
+            .linha {
+                font-family: italic;
+                color: lightblue;
+                padding-left: 10px;
+                line-height: 30px
+            }
+        </style>
     </head>
 
     <body>
-
         <?php
             for($i = 0; $i < 10; $i++){
-                print("Linha Nº " . $i . "<br />");
+                print("<span class =\"linha\"> Linha Nº" . $i . "<span><br />");
             }
         ?>
-        
     </body>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            alert("Prontinho!");
+        });
+    </script>
 </html>
